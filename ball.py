e6805e55f947c3d77e46f54f09c84741eddc6a82
alina <alkovryga@gmail.com>
2024-02-08 22:08:55 +0200

    add empty functions and attribute to class Ball

--- ball.py
+++ ball.py
@@ -1,2 +1,38 @@
 import pygame
 
+
+class Ball:
+    def __init__(self, size: int, speed: int, color: int, coordX: int, coordY: int):
+        self.size = size
+        self.speed = speed
+        self.color = color
+        self.coordX = coordX
+        self.coordY = coordY
+
+    def getSize(self):
+        pass
+
+    def getColor(self):
+        pass
+
+    def setSpeed(self, new_speed: int):
+       pass
+
+    def getSpeed(self):
+       pass
+
+    def getCollision(self):
+        pass
+
+    def checkCollisionBrick(self):
+       pass
+
+    def move(self):
+        pass
+
+    def coordinatesX(self):
+        pass
+
+    def coordinatesY(self):
+        pass
+
