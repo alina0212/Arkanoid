c3e5c2bdfe71dd167c13c0e3faa40317d04277bb
Me <ivanetspolina@knu.ua>
2024-02-22 23:07:46 +0200

    comments

--- game_py/ball.py
+++ game_py/ball.py
@@ -18,37 +18,45 @@ class Ball:
         self.dx = 0
         self.dy = 0
         self.speed = self.receive_speed()
-        self.color_name = "white"
+        self.color = "white"
         self.ball_rect = self.initial_position()
         self.rect = pygame.Rect(self.ball_rect)
 
+    # малюємо м'яч
+    def draw_ball(self):
+        pygame.draw.circle(self.screen, self.color, self.ball_rect.center, self.ball_radius)
+        return True
+
+    # встановлюємо початкову позицію м'яча
+    def initial_position(self):
+        ball_r = int(self.ball_radius * 2 ** 0.5)  # визначаємо м'яч, як квадрат
+
+        # встановлюємо (посередині плаформи, відстань між платф. і м'ячем - діаметр м'яча, ширина, висота)
+        ball_rect = pygame.Rect(self.paddle.rect.centerx - self.ball_radius, self.paddle.rect.top - ball_r, ball_r,
+                                ball_r)
+
+        return ball_rect
+
+    # початковий рух м'яча
+    # визначаємо, щоб м'яч рухався вертикально
     def start_move(self):
         if self.dx == 0 and self.dy == 0:
             self.dx = 0
             self.dy = -1
             self.speed = self.receive_speed()
 
-    def size(self, radius):
-        if radius > 0:
-            self.ball_radius = radius
-            return True
-        else:
-            return False
-
-    def color(self):
-        color = pygame.Color(self.color_name)
-        return color
-
-    def draw_ball(self):
-        pygame.draw.circle(self.screen, self.color(), self.ball_rect.center, self.ball_radius)
-
+    # рух м'яча
     def move(self):
+        # оновлюються координати м'яча
         self.ball_rect.x += self.speed * self.dx
         self.ball_rect.y += self.speed * self.dy
 
+        # координати верхнього лівого кута прямокутника, що обмежує м'яч
+        # використовуємо для колізії з блоками
         self.rect.x = self.ball_rect.x
         self.rect.y = self.ball_rect.y
 
+    # обираємо швидкість м'яча
     def receive_speed(self):
         if self.difficulty == 1:
             return self.speed_1
@@ -56,15 +64,13 @@ class Ball:
             return self.speed_2
         elif self.difficulty == 3:
             return self.speed_3
-        else:
-            return self.speed_1
 
     def collision(self):
         # колізія з правою, лівою межею
         if self.ball_rect.centerx < self.ball_radius or self.ball_rect.centerx > self.width - self.ball_radius:
             self.dx = -self.dx
 
-        # # колізія з верхнею межею
+        # колізія з верхнею межею
         if self.ball_rect.centery < self.ball_radius:
             self.dy = -self.dy
             if self.dx == 0:
@@ -74,40 +80,33 @@ class Ball:
         if self.ball_rect.colliderect(self.paddle.rect) and self.dy > 0:
             self.dy = -self.dy
 
+    # перевіряємо колізію з блоками
     def check_collision_brick(self):
         hits = pygame.sprite.spritecollide(self, self.brick_sprites, True)
         for hit in hits:
-            # Змінюємо напрямок руху м'яча при зіткненні з блоком
-            if self.dx > 0:
-                # Якщо м'яч рухався вправо, змінюємо напрямок наліво
+            if self.dx > 0:  # якщо м'яч рухався вправо, змінюємо напрямок наліво
                 self.dx = -self.dx
-            elif self.dx < 0:
-                # Якщо м'яч рухався вліво, змінюємо напрямок направо
+            elif self.dx < 0:  # якщо м'яч рухався вліво, змінюємо напрямок направо
                 self.dx = -self.dx
 
-            if self.dy > 0:
-                # Якщо м'яч рухався вниз, змінюємо напрямок вгору
+            if self.dy > 0:  # якщо м'яч рухався вниз, змінюємо напрямок вгору
                 self.dy = -self.dy
-            elif self.dy < 0:
-                # Якщо м'яч рухався вгору, змінюємо напрямок вниз
+            elif self.dy < 0:  # якщо м'яч рухався вгору, змінюємо напрямок вниз
                 self.dy = -self.dy
 
-            # Видаляємо блок зі спрайт-групи
+            # видаляємо блок із групи
             self.brick_sprites.remove(hit)
 
         return self.dx, self.dy
 
-    def all_bricks_broken(self): # Перевіряємо, чи всі блоки розбиті
+    # перевіряємо, чи всі блоки розбиті
+    def all_bricks_broken(self):
         return len(self.brick_sprites) == 0
 
+    # перевіряємо, чи доторкнувся м'яч нижньої межі
     def check_collision_bottom(self):
         if self.ball_rect.bottom >= self.height:
             return True
         return False
 
 
-    def initial_position(self):
-        ball_r = int(self.ball_radius * 2 ** 0.5)
-        ball_rect = pygame.Rect(self.paddle.rect.centerx - self.ball_radius, self.paddle.rect.top - ball_r, ball_r,
-                                ball_r)
-        return ball_rect
--- game_py/main.py
+++ game_py/main.py
@@ -34,11 +34,11 @@ class GameWindow:
                         ball.start_move()
 
             paddle.move()
-
             ball.move()
             ball.collision()
             ball.check_collision_brick()
-            # М'яч доторкнувся нижньої межі екрануабо розбиті всі блоки, гра завершена
+
+            # м'яч доторкнувся нижньої межі екрану або розбиті всі блоки, гра завершена
             if ball.check_collision_bottom() or ball.all_bricks_broken():
                 result_window.run()
                 return
@@ -87,87 +87,6 @@ class ResultWindow:
             clock.tick(60)
 
 
-class Button:
-    def __init__(self, screen, x, y, width, height, text, color, action=None):
-        self.default_color = color
-        self.screen = screen
-        self.rect = pygame.Rect(x, y, width, height)
-        self.text = text
-        self.color = color
-        self.action = action
-        self.last_click_time = 0
-        self.click_duration = 0.25
-
-    def draw(self):
-        self.update()
-        pygame.draw.rect(self.screen, self.color, self.rect, border_radius=30)
-        font = pygame.font.SysFont(None, 32)
-        text = font.render(self.text, True, (32, 33, 33))
-        text_rect = text.get_rect(center=self.rect.center)
-        self.screen.blit(text, text_rect)
-
-    def update(self):
-        if time.time() - self.last_click_time < self.click_duration:
-            self.color = (245, 255, 230)
-        else:
-            self.color = self.default_color
-
-    def is_clicked(self):
-        mouse_pos = pygame.mouse.get_pos()
-        clicked = self.rect.collidepoint(mouse_pos)
-        if clicked:
-            self.last_click_time = time.time()
-        return clicked
-
-
-class Game:
-    def __init__(self):
-        self.width = 800
-        self.height = 600
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("Game")
-
-    def run(self):
-        running = True
-        game_window = None
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if difficult1_button.is_clicked():
-                        game_window = GameWindow(difficulty=1)
-
-                    elif difficult2_button.is_clicked():
-                        game_window = GameWindow(difficulty=2)
-
-                    elif difficult3_button.is_clicked():
-                        game_window = GameWindow(difficulty=3)
-
-                    # перехід у вікно гри
-                    if start_button.is_clicked():
-                        if game_window is not None:
-                            game_window.run()
-
-                    # перехід у вікно історії результатів
-                    elif history_results_button.is_clicked():
-                        history_results_window.run()
-
-            self.screen.fill((26, 45, 115))
-            background_image_start = pygame.image.load('../image/ARKANOID.png')
-            resized_image = pygame.transform.scale(background_image_start, (700, 200))
-            game.screen.blit(resized_image, (55, 0))
-            start_button.draw()
-            history_results_button.draw()
-            difficult1_button.draw()
-            difficult2_button.draw()
-            difficult3_button.draw()
-            pygame.display.flip()
-
-        # При виході з циклу гри виходимо з програми
-        pygame.quit()
-
-
 class HistoryResultsWindow:
     def __init__(self):
         self.results = []
@@ -193,10 +112,12 @@ class HistoryResultsWindow:
             pygame.display.flip()
             clock.tick(60)
 
+    # додаємо результати
     def add_result(self, time_spent, blocks_broken):
         self.results.append((time_spent, blocks_broken))
         return self.results
 
+    # відображаємо результати
     def display_results(self, screen_history, result_label_font):
         label_surface = result_label_font.render("Results:", True, (255, 255, 255))
         screen_history.blit(label_surface, (10, 10))
@@ -205,6 +126,88 @@ class HistoryResultsWindow:
             result_text = f"Result #{i + 1}: Time Spent: {result[0]}, Blocks Broken: {result[1]}"
             result_surface = result_label_font.render(result_text, True, (255, 255, 255))
             screen_history.blit(result_surface, (10, 40 + i * 20))
+
+
+class Button:
+    def __init__(self, screen, x, y, width, height, text, color):
+        self.default_color = color
+        self.screen = screen
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = color
+        self.last_click_time = 0
+        self.click_duration = 0.25
+
+    # малюємо кнопку
+    def draw(self):
+        self.update()
+        pygame.draw.rect(self.screen, self.color, self.rect, border_radius=30)
+        font = pygame.font.SysFont(None, 32)
+        text = font.render(self.text, True, (32, 33, 33))
+        text_rect = text.get_rect(center=self.rect.center)
+        self.screen.blit(text, text_rect)
+
+    # оновлює колір кнопки
+    def update(self):
+        if time.time() - self.last_click_time < self.click_duration:
+            self.color = (245, 255, 230)
+        else:
+            self.color = self.default_color
+
+    # перевіряємо, чи мишка натиснула і якщо так, то оновлюємо час кліку
+    def is_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        clicked = self.rect.collidepoint(mouse_pos)
+        if clicked:
+            self.last_click_time = time.time()
+        return clicked
+
+
+class Game:
+    def __init__(self):
+        self.width = 800
+        self.height = 600
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Game")
+
+    def run(self):
+        running = True
+        game_window = None
+        while running:
+            self.screen.fill((26, 45, 115))
+            background_image_start = pygame.image.load('../image/ARKANOID.png')
+            resized_image = pygame.transform.scale(background_image_start, (700, 200))
+            game.screen.blit(resized_image, (55, 0))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    # обираємо складність
+                    if difficult1_button.is_clicked():
+                        game_window = GameWindow(difficulty=1)
+
+                    elif difficult2_button.is_clicked():
+                        game_window = GameWindow(difficulty=2)
+
+                    elif difficult3_button.is_clicked():
+                        game_window = GameWindow(difficulty=3)
+
+                    # перехід у вікно гри
+                    if start_button.is_clicked():
+                        if game_window is not None:
+                            game_window.run()
+
+                    # перехід у вікно історії результатів
+                    elif history_results_button.is_clicked():
+                        history_results_window.run()
+
+            start_button.draw()
+            history_results_button.draw()
+            difficult1_button.draw()
+            difficult2_button.draw()
+            difficult3_button.draw()
+            pygame.display.flip()
 
 
 pygame.init()
@@ -224,3 +227,4 @@ result_window = ResultWindow()
 
 game.run()
 
+pygame.quit()
